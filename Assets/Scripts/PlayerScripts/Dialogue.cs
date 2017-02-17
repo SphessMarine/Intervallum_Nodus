6f76cab1e88413434f93a5deb26c7d3c72a8e0b9
SphessMarine <sm03755@gmail.com>
2017-02-17 17:27:26 -0500

    Added proper script directories and added creepy text spawning.

---
+++ Assets/Scripts/PlayerScripts/Dialogue.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+
+   #region LocalVariables
+
+   public GameObject dText = null;
+   public Text dialogueText = null;
+   private bool runCoroutine = false;
+   private bool spawn = false;
+   const float maxAlpha = 1.0f;
+   const float minAlpha = 0.0f;
+   float curAlpha = 0.0f;
+   const float deltaAlpha = 0.01f;
+   private Color c = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+   private Color cText = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+   #endregion
+
+   #region TriggerEvents
+
+   /// <summary>
+   /// Raise OnTriggerEnter event.
+   /// Spawn dialogue window with recieved text
+   /// </summary>
+   /// <param name="other"></param>
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.GetComponent<DialogueTrigger>())
+      {
+         dText.SetActive(true);
+         dialogueText.text = other.gameObject.GetComponent<DialogueTrigger>().TriggerText;
+         StopCoroutine(SpawnText());
+         spawn = true;
+         StartCoroutine(SpawnText());
+      }
+   }
+   
+   /// <summary>
+   /// Raise OnTriggerExit event.
+   /// Despawn dialogue window and reset text
+   /// </summary>
+   /// <param name="other"></param>
+   void OnTriggerExit(Collider other)
+   {
+      if (other.gameObject.GetComponent<DialogueTrigger>())
+      {
+         StopCoroutine(SpawnText());
+         spawn = false;
+         StartCoroutine(SpawnText());
+      }
+   }
+
+   #endregion
+
+   /// <summary>
+   /// Spawns the text in a spooky fashion.
+   /// </summary>
+   /// <returns></returns>
+   private IEnumerator SpawnText()
+   {
+      // Set our conditional
+      runCoroutine = true;
+      // Reset color to set with the minAlpha
+      c = new Color(0.0f, 0.0f, 0.0f, curAlpha);
+      cText = new Color(1.0f, 1.0f, 1.0f, curAlpha);
+      // Set dText object's color to our new color
+      dText.GetComponent<Image>().color = c;
+      dialogueText.color = cText;
+      while (runCoroutine)
+      {
+         if (spawn)
+         {
+            // While the current alpha is less than the max, iterate by delta
+            if (curAlpha < maxAlpha)
+            {
+               curAlpha = c.a + deltaAlpha;
+            }
+            // Otherwise, we are == or > max and should cease execution
+            else
+            {
+               curAlpha = maxAlpha;
+               runCoroutine = false;
+            }
+         }
+         else
+         {
+            // While the current alpha is > the min, iterate by delta
+            if (curAlpha > minAlpha)
+            {
+               curAlpha = c.a - deltaAlpha;
+            }
+            // Otherwise, we are == or < min and should cease execution
+            else
+            {
+               curAlpha = minAlpha;
+               runCoroutine = false;
+            }
+         }
+         c = new Color(0.0f, 0.0f, 0.0f, curAlpha);
+         cText = new Color(1.0f, 1.0f, 1.0f, curAlpha);
+         // Set dText color to new color
+         dText.GetComponent<Image>().color = c;
+         dialogueText.color = cText;
+         yield return new WaitForEndOfFrame();
+      }
+      if (!spawn)
+      {
+         dText.SetActive(false);
+         curAlpha = 0.0f;
+      }
+      else
+      {
+         curAlpha = 1.0f;
+      }
+      yield return null;
+   }
+
+}
---
+++ Assets/Scripts/UIScripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+   public Transform player;
+
+   // Update is called once per frame
+   void Update ()
+   {
+      transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+   }
+}
